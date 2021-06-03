1c368c239250610c99e9def0f7067c0e7c0a4741
nerotruuv <mark.steijger@gmail.com>
2021-06-03 14:55:59 +0200

    added a navigation file for a* pathing

---
+++ Code/navigation.py
@@ -0,0 +1,77 @@
+
+"""
+This script shows the basic use of the MotionCommander class.
+
+Simple example that connects to the crazyflie at `URI` and runs a
+sequence. This script requires some kind of location system, it has been
+tested with (and designed for) the flow deck.
+
+The MotionCommander uses velocity setpoints.
+
+Change the URI variable to your Crazyflie configuration.
+"""
+import logging
+import time
+
+import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.positioning.motion_commander import MotionCommander
+from cflib.utils import uri_helper
+
+import pathfinding
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+
+URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
+
+# Only output errors from the logging framework
+logging.basicConfig(level=logging.ERROR)
+
+#maak dit minimaal 30*30
+map = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+]
+# schrijf een functie voor aanpassen van coordinaten / nieuwe objecten
+# schrijf een functie voor weghalen van obkjecten 
+# schrijf een functie voor het wegscchrijven van gebruikte paden
+# schrijf een functie voor het dynamish wegahalen van paden nadat ze zijn gevlogen
+# schrijf een functie voor het verplaatsen van end goal
+# schrijf een functie voor verweideren van een end goal
+
+#potentie?
+
+grid = Grid(matrix=map)
+start = grid.node(0, 0)
+end = grid.node(6, 6)
+
+finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
+path, runs = finder.find_path(start, end, grid)
+
+print('operations:', runs, 'path length:', len(path))
+print(path)
+# print(grid.grid_str(path=path, start=start, end=end))
+
+# if __name__ == '__main__':
+    # grid = Grid(matrix=map)
+#     # Initialize the low-level drivers
+#     cflib.crtp.init_drivers()
+
+#     with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
+#         # We take off when the commander is created
+#         with MotionCommander(scf) as mc:
+
+
