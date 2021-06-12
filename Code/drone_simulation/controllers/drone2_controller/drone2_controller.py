45a0d3d88ddf7d84f324767f8882084a1281175b
Robert Karajev <0851997@hr.nl>
2021-06-12 14:59:10 +0200

    Add first version of the simulation

---
+++ Code/drone_simulation/controllers/drone2_controller/drone2_controller.py
@@ -0,0 +1,82 @@
+from controller import Supervisor
+import sys
+sys.path.append('C:\\Users\\Robert\\Desktop\\TINLabs AS\\Code\\drone_simulation\\controllers')
+import lib.services as sv
+import lib.credentials as cr
+from multiprocessing import Process
+
+# define grid specifications
+grid_length = 1
+grid_tile_size = 0.1
+offset = [0.05, 0.005, 0.05]
+
+# initiate the Supervisor and handler for movement of the drone.
+drone = Supervisor()
+drone_node = drone.getSelf()
+translation_handler = drone_node.getField("position")
+
+# # get the time step of the current world.
+timestep = int(drone.getBasicTimeStep())
+
+# create communication link using mqtt protocol
+clientName = 'drone_2'
+assigned_topics = [clientName + '_instructions']
+credentials = cr.getCredentials()
+mqttClient = sv.MqttClient(credentials[0], credentials[1], credentials[2], credentials[3])
+mqttClient.createClient(clientName, assigned_topics)
+mqttClient.startConnection()
+
+# variable used for processing a single path step
+processing_message = False
+
+# create PositionHandler to handle movement to received position
+positionHandler = sv.PositionHandler([0, 0, 0], offset)
+positionHandler.generateAbsoluteGrid(grid_length, grid_tile_size)
+current_position = []
+
+# variables used for drone control
+flight_height = 0.3                                         # m
+drone_speed = 0.1                                           # m/s
+simulated_drone_speed = timestep * drone_speed / 1000       # m/0.032s
+
+while drone.step(timestep) != -1:
+    # this block will processs all messages received one by one
+    if not len(mqttClient.messages) <= 0 and not processing_message:
+        print('Total messages: ', len(mqttClient.messages))
+        message = mqttClient.messages.pop(0)
+        print('Current message: ', message)
+        print('')
+    
+        # absolute position updated based on the received command + position
+        x = positionHandler.getDestCoordX(message['body']['position'][0])
+        z = positionHandler.getDestCoordZ(message['body']['position'][1])
+        print('target coords: ', (x, z))
+
+        processing_message = True
+
+    if processing_message:
+        current_position = drone_node.getPosition()
+        # print(current_position)
+        
+        if message['body']['state'] == 'on':
+            current_position = [x, positionHandler.start_position[1], z]
+            processing_message = False
+        elif message['body']['state'] == 'takeoff':
+            if current_position[1] <= flight_height:
+                current_position[1] += simulated_drone_speed
+            else:
+                processing_message = False
+        elif message['body']['state'] == 'moving':
+            if current_position[0] <= x:
+                current_position[0] += simulated_drone_speed
+            elif current_position[2] <= z:
+                current_position[2] += simulated_drone_speed
+            else:
+                processing_message = False
+        elif message['body']['state'] == 'landing':
+            if current_position[1] >= positionHandler.start_position[1]:
+                current_position[1] -= simulated_drone_speed
+            else:
+                processing_message = False
+        
+        translation_handler.setSFVec3f(current_position)
---
+++ Code/drone_simulation/controllers/lib/services.py
@@ -0,0 +1,108 @@
+import json
+import paho.mqtt.client as mqtt
+from multiprocessing import Process
+
+class PositionHandler:
+    def __init__(self, position, offset):
+        self.offset = offset
+        self.start_position = [sum(x) for x in zip(position, offset)]
+
+    def generateAbsoluteGrid(self, length, tile_size):
+        self.absolute_grid = int (length / tile_size)
+
+    def getDestCoordX(self, x):
+        if x > self.absolute_grid-1 or x < 0:
+            raise Exception('Please assign grid value from the following range [0 - 9]')
+        else:
+            dest_coord_x = x / self.absolute_grid + self.offset[0]
+            return dest_coord_x
+
+    def getDestCoordZ(self, z):
+        if z > self.absolute_grid-1 or z < 0:
+            raise Exception('Please assign grid value from the following range [0 - 9]')
+        else:
+            dest_coord_z = z / self.absolute_grid + self.offset[0]
+            return dest_coord_z
+
+
+class MqttClient:
+    # initialize MqttClient for communication with Mqtt broker
+    def __init__(self, user, password, port, brokerAddress='127.0.0.1', logger=None):
+        self.brokerAddress = brokerAddress  # Broker address
+        self.port = port                    # Broker port
+        self.user = user                    # Connection username
+        self.password = password            # Connection password
+
+        self.messages = []
+
+    # create new instace that will communicate the Mqtt broker
+    def createClient(self, name, topics):
+        self.name = name
+        self.assigned_topics = topics
+        self.client = mqtt.Client(self.name)
+        self.client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
+        self.client.username_pw_set(self.user, password=self.password)
+        
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        
+        print(f'[{self.name}] Instance of MQTT Client created.')
+
+    # start connection of MqttClient
+    def startConnection(self):
+        try:
+            self.client.connect(self.brokerAddress, port=self.port)
+
+            # create new thread to process network traffic
+            self.client.loop_start()
+            
+            print(f'[{self.name}] Connection established.')
+        except:
+            print(f'[{self.name}] Connection failed, retrying...')
+
+    # method which stops all initialized object to properly close the MqttClient connection
+    def stopConnection(self):
+        self.client.disconnect()
+        self.client.loop_stop()
+        print(f'[{self.name}] Connection closed.')
+        
+    # method which publishes a created message to Mqtt Broker
+    def sendPublish(self, topic, message, qos):
+        print('The following message is sent :', '(topic: '+topic+', message: '+str(message)+', qos: '+str(qos)+')')
+        self.client.publish(topic, json.dumps(message), qos)
+
+    # callback method which is used on connect to subscribed to perticular channels found within this method
+    def on_connect(self, client, userdata, flags, connectionResult):
+        errorMessage = f'[{self.name}] Connection refused'
+        if connectionResult == 0:
+            for topic in self.assigned_topics:
+                self.client.subscribe(topic, 1)
+        elif connectionResult == 1:
+            print(errorMessage, 'incorrect protocol version')
+        elif connectionResult == 2:
+            print(errorMessage, 'invalid client identifier')
+        elif connectionResult == 3:
+            print(errorMessage, 'server unavailable')
+        elif connectionResult == 4:
+            print(errorMessage, 'bad username or password')
+        elif connectionResult == 5:
+            print(errorMessage, 'not authorised')
+
+    # this method triggers an event when a message is received
+    def on_message(self, client, userdata, message):
+        # messages are received as MQTTMessage object
+        print('message received')
+        topic = message.topic
+        message = str(message.payload.decode('utf-8'))
+        message = json.loads(message)
+        self.messages.append(message)
+
+if __name__ == '__main__':
+    x = 1
+    y = 1 
+    z = 3
+    offset = [0.05, 0.005, 0.05]
+    ph = PositionHandler([0, 0, 0], offset)
+    ph.generateAbsoluteGrid(1, 0.1)
+    ph.updateAbsolutePosition(x, z)
+    ph.updateAbsolutePosition(x, y, z)
