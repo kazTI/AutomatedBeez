3edb81764eea71925ad4dbc101575dfc29bdedac
Robert Karajev <0851997@hr.nl>
2021-06-18 22:27:44 +0200

    update

--- Code/camera/pi_color_detectionV2.py
+++ Code/camera/pi_color_detectionV2.py
@@ -16,24 +16,24 @@ def nothing(x):
 def mouseClicked(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
         if y > button[0] and y < button[1] and x > button[2] and x < button[3]:   
-            # masks.append()
-            # generateMask(hsv)
-            print('Butten Clicked !!!')
+            masks.append(mask)
+            print(masks)
 
             
 def generateMask(hsv):
     B = cv2.getTrackbarPos('B', 'Color Calibration')
     G = cv2.getTrackbarPos('G', 'Color Calibration')
     R = cv2.getTrackbarPos('R', 'Color Calibration')
+    
     rgb_color = np.uint8([[[B, G, R]]])
     hsv_color = cv2.cvtColor(rgb_color ,cv2.COLOR_BGR2HSV)
     lowerLimit = np.uint8([hsv_color[0][0][0]-10,100,100])
     upperLimit = np.uint8([hsv_color[0][0][0]+10,255,255])
-    
     mask = cv2.inRange(hsv, lowerLimit, upperLimit)
     
-    print(hsv_color)
     cv2.imshow('Mask', mask)
+    
+    return mask
 
 
 
@@ -43,13 +43,12 @@ camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 30
 rawCapture = PiRGBArray(camera, size=(640, 480))
-global image
-global hsv
 
 
 # initialize window for calibration
+masks = []
 cv2.namedWindow('Color Calibration')
-cv2.setMouseCallback('Color Calibration', mouseClicked)
+cv2.setMouseCallback('Color Calibration', mouseClicked, )
 cv2.createTrackbar('B', 'Color Calibration', 0, 255, nothing)
 cv2.createTrackbar('G', 'Color Calibration', 0, 255, nothing)
 cv2.createTrackbar('R', 'Color Calibration', 0, 255, nothing)
@@ -61,13 +60,13 @@ button_background[button[0]:button[1],button[2]:button[3]] = 180
 cv2.putText(button_background, 'Next', (100,50), cv2.FONT_HERSHEY_PLAIN, 2, (0), 3)
 cv2.imshow('Color Calibration', button_background)
 
-
+global mask
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
     cv2.imshow("Camera stream", image)
 
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    generateMask(hsv)
+    mask = generateMask(hsv)
 
 
     rawCapture.truncate(0)
