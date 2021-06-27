8eac6b613694b575ed78ba6c2065ce2126d26694
robertkarajev <33599801+robertkarajev@users.noreply.github.com>
2021-06-27 09:55:01 +0200

    Delete credentials.py

--- Code_clean/lib/credentials.py
+++
@@ -1,5 +0,0 @@
-# def getCredentials():
-#     return ['CoS-Server', 'Mqttserver123', 8883, '5c2b60e68aba4438be4adec764576565.s1.eu.hivemq.cloud']
-
-def getCredentials():
-    return ['server', 'Server123', 8883, '60107eff2102442ba26bb111851b700c.s2.eu.hivemq.cloud']
