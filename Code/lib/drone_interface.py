8aaf82547edf4f5e7b8876b8189de92cf33c8857
kazTI <33227382+kazTI@users.noreply.github.com>
2021-06-30 15:04:55 +0200

    Add files via upload

--- Code/lib/drone_interface.py
+++ Code/lib/drone_interface.py
@@ -1,3 +1,4 @@
+
 import sys
 import lib.services as sv
 import lib.credentials as cr
@@ -15,165 +16,93 @@ from cflib.positioning.motion_commander import MotionCommander
 from cflib.utils import uri_helper
 from cflib.crazyflie.syncLogger import SyncLogger
 
-class Periferal:
-    def __init__(self, URI):
-        self.URI = URI
-
-    def wait_for_position_estimator(self, scf):
-        print('Waiting for estimator to find position...')
-
-        log_config = LogConfig(name='Kalman Variance', period_in_ms=500)
-        log_config.add_variable('kalman.varPX', 'float')
-        log_config.add_variable('kalman.varPY', 'float')
-        log_config.add_variable('kalman.varPZ', 'float')
-
-        var_y_history = [1000] * 10
-        var_x_history = [1000] * 10
-        var_z_history = [1000] * 10
-
-        threshold = 0.001
-
-        with SyncLogger(scf, log_config) as logger:
-            for log_entry in logger:
-                data = log_entry[1]
-
-                var_x_history.append(data['kalman.varPX'])
-                var_x_history.pop(0)
-                var_y_history.append(data['kalman.varPY'])
-                var_y_history.pop(0)
-                var_z_history.append(data['kalman.varPZ'])
-                var_z_history.pop(0)
-
-                min_x = min(var_x_history)
-                max_x = max(var_x_history)
-                min_y = min(var_y_history)
-                max_y = max(var_y_history)
-                min_z = min(var_z_history)
-                max_z = max(var_z_history)
-
-                # print("{} {} {}".
-                #       format(max_x - min_x, max_y - min_y, max_z - min_z))
-
-                if (max_x - min_x) < threshold and (
-                        max_y - min_y) < threshold and (
-                        max_z - min_z) < threshold:
-                    break
-
-
-    def reset_estimator(self, scf):
-        cf = scf.cf
-        cf.param.set_value('kalman.resetEstimation', '1')
-        time.sleep(0.1)
-        cf.param.set_value('kalman.resetEstimation', '0')
-        self.wait_for_position_estimator(scf)
-
-
-    def activate_high_level_commander(self, scf):
-        scf.cf.param.set_value('commander.enHighLevel', '1')
-
-
-    def activate_mellinger_controller(self, scf, use_mellinger):
-        controller = 1
-        if use_mellinger:
-            controller = 2
-        scf.cf.param.set_value('stabilizer.controller', controller)
-
 class DroneInterface:
     DEFAULT_HEIGHT = 0.3
     flightTime = 2
-    wait_time = 1.5
-    def __init__(self, name, URI, scf):
+    def __init__(self, name, URI):
         cflib.crtp.init_drivers()
         self.URI = URI
-        self.periferal = Periferal(self.URI)
-        self.cells = 5
+        self.cells = 10
         self.message = ''
-        self.scf = scf
+        self.connected = ''
+        self.flying_message = False
 
         self.drone_start_position = []
         self.drone_current_position = []
-        self.drone_destination = None
         self.name = name
         self.ready = False
 
-        self.drone_states = ['available', 'idle', 'scouting', 'returning' 'dancing', 'gathering']
-        self.drone_state = self.drone_states[0]
-        self.scout = False
-        self.bussy = False
+        
+        self.drone_state = 'available'
         self.flying = False
-        self.ready = False
 
         self.credentials = cr.getCredentials()
-        self.mqttClient = sv.MqttClient(self.credentials[0], self.credentials[1], self.credentials[2], self.credentials[3])
-        self.mqttClient.createClient('drone', ['drone',])
-        self.mqttClient.startConnection()
+        self.orders_mqttClient = sv.MqttClient(self.credentials[0], self.credentials[1], self.credentials[2], self.credentials[3])
+        self.orders_mqttClient.createClient('drone_control', ['drone_control',])
+        self.orders_mqttClient.startConnection()
+        self.connect = URI, name
+        self.message_connect = self.createMessageDrone(None, self.connect)
+        self.orders_mqttClient.sendPublish('crazyflie_control', self.message_connect, 0)
+        while self.connected == '':
+            try:
+                _, message = self.orders_mqttClient.messages.pop(0)
+                self.connected = message['connection']
+            except:
+                pass
+        self.position_mqttClient = sv.MqttClient(self.credentials[0], self.credentials[1], self.credentials[2], self.credentials[3])
+        self.position_mqttClient.createClient('drone', ['drone',])
+        self.position_mqttClient.startConnection()
         position_thread = td.Thread(target=self.getCurrentPosition, daemon = True)
         position_thread.start()
 
     def getCurrentPosition(self):
         while True:
-            if not len(self.mqttClient.messages) <= 0:
-                _, self.message = self.mqttClient.messages.pop(0)
-                #print('Message from controller: ', self.message[self.name])
+            if not len(self.position_mqttClient.messages) <= 0:
+                _, self.message = self.position_mqttClient.messages.pop(0)
                 self.drone_current_position = self.message[self.name]
-                self.ready = True
-                self.mqttClient.messages = []
+                self.position_mqttClient.messages = []
                 self.ready = True
                 time.sleep(0.2)
 
-    def createMessage(self, state, nextPosition):
+    def createMessageDrone(self, orders=None, connect = None):
         message =   {
-                        "state": self.drone_states[state],
-                        "position": nextPosition,
+                        "connection": connect,
+                        "orders": orders,
                     }
         return message
 
     def takeoff(self):
-        # with SyncCrazyflie(self.URI, cf=Crazyflie(rw_cache='./cache')) as scf:
-            self.mqttClient.sendPublish('real_drone_instructions', ('takeoff', []), 0)
-            self.bussy = True
-            self.periferal.activate_high_level_commander(self.scf)
-            self.periferal.reset_estimator(self.scf)
-            self.periferal.activate_mellinger_controller(self.scf, False)
-            commander = self.scf.cf.high_level_commander
-            commander.takeoff(DroneInterface.DEFAULT_HEIGHT, 1.5)
-            time.sleep(DroneInterface.flightTime)
-            self.bussy = False
-            self.flying = True
+        print('yo')
+        orders = self.name, 'takeoff'
+        message_drone = self.createMessageDrone(orders, None)
+        self.orders_mqttClient.sendPublish('crazyflie_control', message_drone, 0)
+        while not self.flying_message:
+            try:
+                _, message = self.orders_mqttClient.messages.pop(0)
+                self.flying_message = message['flying']
+            except:
+                pass
+        self.flying = True
 
     def droneMove(self, destination):
-        #with SyncCrazyflie(self.URI, cf=Crazyflie(rw_cache='./cache')) as self.scf:
-            x = (destination[0] - self.drone_current_position[0]) / self.cells / 2
-            y = - (destination[1] - self.drone_current_position[1]) / self.cells / 2
-            self.periferal.activate_mellinger_controller(self.scf, False)
-            commander = self.scf.cf.high_level_commander
-            commander.go_to(x, y, 0, 0, DroneInterface.flightTime, relative=True)
-            time.sleep(0.2)
+        x = (destination[0] - self.drone_current_position[0]) / self.cells / 2
+        y = - (destination[1] - self.drone_current_position[1]) / self.cells / 2
+        orders = self.name, 'move', x, y
+        message_drone = self.createMessageDrone(orders, None)
+        self.orders_mqttClient.sendPublish('crazyflie_control', message_drone, 0)
+        time.sleep(0.2)
 
     def droneDance(self):
-        # with SyncCrazyflie(self.URI, cf=Crazyflie(rw_cache='./cache')) as scf:
-            self.mqttClient.sendPublish('real_drone_instructions', ('dance', []), 0)
-            self.bussy = True
-            self.periferal.activate_mellinger_controller(self.scf, False)
-            commander = self.scf.cf.high_level_commander
-            commander.go_to(0, 0, 0, 2*math.pi, 3*DroneInterface.flightTime, relative=True)
-            time.sleep(3*DroneInterface.flightTime)
-            commander.go_to(0, 0, 0, -2*math.pi, 3*DroneInterface.flightTime, relative=True)
-            time.sleep(3*DroneInterface.flightTime)
-            if self.drone_start_position != self.drone_current_position:
-                x = (self.drone_start_position[0] - self.drone_current_position[0]) / self.cells / 2
-                y = - (self.drone_start_position[1] - self.drone_current_position[1]) / self.cells / 2
-                commander.go_to(x, y, 0, 0, DroneInterface.flightTime, relative=True)
-                time.sleep(DroneInterface.flightTime)
-            self.bussy = False
+        orders = self.name, 'dance'
+        message_drone = self.createMessageDrone(orders, None)
+        self.orders_mqttClient.sendPublish('crazyflie_control', message_drone, 0)
+        time.sleep(6*DroneInterface.flightTime)
 
     def droneLand(self):
-        # with SyncCrazyflie(self.URI, cf=Crazyflie(rw_cache='./cache')) as scf:
-            self.mqttClient.sendPublish('real_drone_instructions', ('land', []), 0)
-            self.periferal.activate_mellinger_controller(scf, False)
-            commander = self.scf.cf.high_level_commander
-            commander.land(0.0, DroneInterface.flightTime)
-            time.sleep(DroneInterface.flightTime)
-            commander.stop()
-            self.flying = False
+        orders = self.name, 'land'
+        message_drone = self.createMessageDrone(orders, None)
+        self.orders_mqttClient.sendPublish('crazyflie_control', message_drone, 0)
+        time.sleep(DroneInterface.flightTime)
+        self.flying_message = False
+        self.flying = False
 
